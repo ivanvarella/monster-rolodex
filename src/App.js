1d8de8606fa027fe9a4ba9b7b2618f2f901662ff
Ivan Varella <ivanvarella@gmail.com>
2023-01-24 11:33:32 -0300

    update title

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Rolodex 2</h1>
+      <h1 className="app-title">Monsters Rolodex</h1>
 
       <SearchBox
         onChangeHandler={onSearchChange}
